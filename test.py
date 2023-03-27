0331917bb3a5595653b9151b243f61d76f1a798d
LL-LLLu <qilutx@gmail.com>
2023-03-27 06:17:45 -0500

    header commit

---
+++ test.py
@@ -0,0 +1,9 @@
+import time
+import keyboard
+
+
+
+while True:
+    keyboard.press_and_release('space')
+    #keyboard.press_and_release('enter')
+    time.sleep(0.1)                                         
